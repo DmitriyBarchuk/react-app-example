40e8344510fa35f8291360d77605ddb0af770a1c
DmitriyBarchuk <developmentiteasy@gmail.com>
2017-02-18 19:17:35 +0200

    Creating a component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
+var Same = React.createClass({
+  render: function() {
+    return (
+      <div className="same">
+        Same text
+      </div>
+    );
+  }
+});
+
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        No news - nothing to comment.
+      </div>
+    );
+  }
+});
+
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+        No News!
+        <Comments />
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Hi all, i am a React component!!!
+        <News />
+        <Same />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
